261c76cd8fc67fa08f5e0c57f12dfb8b88c840b4
alejandrosanesteban <alejandrosanesteban@gmail.com>
2024-07-23 23:25:28 +0200

    last

---
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.data.ts
@@ -0,0 +1,21 @@
+import { Car, Calendar } from "lucide-react";
+
+export const dataGeneralSidebar = [
+    {
+        icon: Car,
+        label: "Cars",
+        href: "/dashboard"
+    },
+
+    {
+        icon: Calendar,
+        label: "Cars Reserves",
+        href: "/reserves"
+    },
+
+    {
+        icon: Car,
+        label: "Cars",
+        href: "/loved-cars"
+    }
+]
--- app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.tsx
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,7 +1,21 @@
 "use client"
+import { Separator } from "@/components/ui/separator"
+import { useAuth } from "@clerk/nextjs"
+import { dataGeneralSidebar } from "./SidebarRoutes.data";
+
 
 export function SidebarRoutes() {
+    const {userId} = useAuth();
     return (
-        <div>SidebarRoutes</div>
+        <div className="flex flex-col justify-between h-full">
+            <div>
+                <div className="p-2 md:p-6">
+                    <p className="mb-2 text-slate-500">GENERAL</p>
+                    {dataGeneralSidebar.map((item) => (
+                        <p>{item.label}</p>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
